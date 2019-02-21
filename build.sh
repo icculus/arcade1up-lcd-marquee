04f99fa471bbbac5e73ad4a8540433fd6561b3dc
Ryan C. Gordon <icculus@icculus.org>
2019-02-21 16:41:42 +0000

    Add Raspberry Pi build options, NEON support, etc.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc -Wall -Os -o marquee-displaydaemon marquee-displaydaemon.c `sdl2-config --cflags --libs`
+gcc -mcpu=cortex-a53 -mfpu=neon-fp-armv8 -mfloat-abi=hard -Wall -Os -o marquee-displaydaemon marquee-displaydaemon.c `sdl2-config --cflags --libs` -lm
