a7994c3cbc6c0b477fefbcd545608fdcb97d89e8
Ryan C. Gordon <icculus@icculus.org>
2019-02-21 15:56:21 -0500

    Initial attempts at onstart/onstop scripts.

---
+++ runcommand-onstop.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# arcade1up-lcd-marquee; control an LCD in a Arcade1Up marquee.
+#
+# Please see the file LICENSE.txt in the source's root directory.
+#
+#  This file written by Ryan C. Gordon.
+
+exec /home/pi/projects/arcade1up-lcd-marquee/marquee-showimage /home/pi/stickers-neo-geo-system.jpg
+
+
