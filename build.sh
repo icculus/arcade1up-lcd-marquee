a66595f93390e9546f2805dd68a2e9dadeb14074
Ryan C. Gordon <icculus@icculus.org>
2019-03-03 02:43:00 +0000

    Ship SDL2 with the project until we can count on the fixes being widely available.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-gcc -mcpu=cortex-a53 -mfpu=neon-fp-armv8 -mfloat-abi=hard -Wall -Os -o marquee-displaydaemon marquee-displaydaemon.c `sdl2-config --cflags --libs` `pkg-config --cflags --libs dbus-1 libevdev` -lm
+gcc -mcpu=cortex-a53 -mfpu=neon-fp-armv8 -mfloat-abi=hard -Wall -Os -o marquee-displaydaemon marquee-displaydaemon.c `sdl2-config --cflags` `pkg-config --cflags --libs dbus-1 libevdev` -lm -Wl,-rpath,\$ORIGIN ./libSDL2-2.0.so.0
+
